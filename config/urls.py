009d58ea02d05605b78b13815af3f06cfef20388
unohayato <mamidori1p1p@gmail.com>
2022-12-07 18:14:02 +0900

    allauth.urls routing

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('snsapp.urls')),
 ]
