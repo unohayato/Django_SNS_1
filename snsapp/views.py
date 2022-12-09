2ffc12d8e5dc8bc4f9be5e66b17b0346bf90295a
unohayato <mamidori1p1p@gmail.com>
2022-12-10 00:23:09 +0900

    Update views.py

--- snsapp/views.py
+++ snsapp/views.py
@@ -8,4 +8,12 @@ class Home(LoginRequiredMixin, ListView):
   template_name = 'list.html'
   
   def get_queryset(self):
-    return Post.objects.filter(user=self.request.user)
+    return Post.objects.exclude(user=self.request.user)
+  
+class MyPost(LoginRequiredMixin, ListView):
+   """自分の投稿のみ表示"""
+   model = Post
+   template_name = 'list.html'
+
+   def get_queryset(self):
+       return Post.objects.filter(user=self.request.user)
