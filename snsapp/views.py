5218c8a3cbcde7d5829538dcadb28e978a46a565
unohayato <mamidori1p1p@gmail.com>
2022-12-10 00:21:43 +0900

    views

--- snsapp/views.py
+++ snsapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+class Home(LoginRequiredMixin, ListView):
+  model = Post
+  template_name = 'list.html'
+  
+  def get_queryset(self):
+    return Post.objects.filter(user=self.request.user)
