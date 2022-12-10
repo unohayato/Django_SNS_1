a1918dd351e56d1d0906ed57e7b1bfd196a45d37
unohayato <mamidori1p1p@gmail.com>
2022-12-10 22:53:46 +0900

    create

--- snsapp/urls.py
+++ snsapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('mypost/', views.MyPost.as_view(), name='mypost'),
     path('detail/<int:pk>', views.DetailPost.as_view(), name='detail'),
+    path('create/', CreatePost.as_view(), name='create'), 
 ]
--- snsapp/views.py
+++ snsapp/views.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from .models import Post
+
+class CreatePost(LoginRequiredMixin, CreateView):
+  model = Post
+  template_name = 'create.html'
+  fields = ['title', 'content']
+  success_url = reverse_lazy('mypost')
+  
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+    
 
 class DetailPost(LoginRequiredMixin, DetailView):
   model = Post
