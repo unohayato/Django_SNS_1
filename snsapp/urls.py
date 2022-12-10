17bd93c871820b653c4340e92d9ca320d7ff01d9
unohayato <mamidori1p1p@gmail.com>
2022-12-10 22:46:17 +0900

    Update urls.py

--- snsapp/urls.py
+++ snsapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('mypost/', views.MyPost.as_view(), name='mypost'),
+    path('detail/<int:pk>', views.DetailPost.as_view(), name='detail'),
 ]
